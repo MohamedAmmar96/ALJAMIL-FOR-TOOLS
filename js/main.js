d930933ffc4775c1f778e7d2c54032521af122f3
Mohamed Ammar <ammarmohamed074@gmail.com>
2020-11-23 10:51:23 +0200

    Edit on buttons hover and small screen sliders

--- js/main.js
+++ js/main.js
@@ -108,7 +108,7 @@ $(document).ready(function() {
         responsiveClass: true,
         responsive: {
             0: {
-                items: 1,
+                items: 2,
                 dots: true,
                 nav: false,
                 loop: true,
